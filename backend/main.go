8c499170747d0ee86ef67762935c9416e4e68465
tin-auppati <tin.auppati@gmail.com>
2026-02-19 13:37:30 +0700

    Config: Enable CORS in backend and update Node version to 22 for Next.js

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,11 @@ func main() {
 	defer DB.Close()
 
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:8080"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -35,7 +41,7 @@ func main() {
 		api.POST("/games/join", JoinGameHandler)
 		api.POST("/games/move", MakeMoveHandler)
 
-		api.GET("/games/:id", GetGameHandler)       // ดูสถานะเกม
+		api.GET("/games/:id", GetGameHandler)            // ดูสถานะเกม
 		api.GET("/games/:id/moves", GetGameMovesHandler) // ดูประวัติ
 	}
 
